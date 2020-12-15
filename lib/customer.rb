b72b6d423fae02179d3bee33b780ebb85964b896
TheNanoBay <noorani.mehdi@outlook.com>
2020-12-15 09:12:52 -0600

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,30 @@
+
 class Customer
+    attr_accessor :name, :age
   
-end
+    @@all = []
+  
+    def initialize(name, age)
+      @name = name
+      @age = age
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip)
+    end 
+
+    def meals
+        Meal.all.select{|i| i.customer == self}
+    end
+
+    def waiters
+        meals.map{|meal| meal.waiter} #calls on own meals instnace method that does part of the work 
+    end
+
+  
+  end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,32 @@
 class Waiter
 
-end
+    attr_accessor :name, :yrs_experience
+  
+    @@all = []
+  
+    def initialize(name, yrs_experience)
+      @name = name
+      @yrs_experience = yrs_experience
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+    end
+
+
+    def meals
+        Meal.all.select{|i| i.waiter == self}
+    end
+
+    def best_tipper
+        #meals.select{|meal| max = meal if meal.tip > max}
+        max = meals.max_by{|meal| meal.tip}
+        max.customer
+    end 
+
+  end
